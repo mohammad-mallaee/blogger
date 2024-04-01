3878d5801bff331d2be2d2329f67055e399386f1
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-04-01 16:42:47 +0330

    update font-wieghts to use tailwind variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,15 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontWeight: {
+        lighter: '300',
+        light: '350',
+        normal: '400',
+        medium: '450',
+        semibold: '500',
+        bold: '600',
+        extrabold: '700',
+      }
     },
   },
   plugins: [],
