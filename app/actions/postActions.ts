cc58fae0fb8a94e1d3f0788814d87ef3ff492b19
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-08-17 21:32:48 +0330

    idk

--- app/actions/postActions.ts
+++ app/actions/postActions.ts
@@ -54,4 +54,60 @@ export async function getPost(slug: string): Promise<{ content: string, data: Po
     if (!data || !data.title)
         throw Error("The file is empty or doesn't have proper headings")
     return { content, data }
+}import { readdir, readFile } from "fs/promises";
+import matter from "gray-matter";
+import config from '../../config.blog'
+import path from "path"
+import { PostData } from "../utils/types"
+
+function readMarkdownFile(path: string, slug: string) {
+    return new Promise<PostData>((resolve, reject) => {
+        readFile(path, 'utf-8').then(fileData => {
+            const { data }: { data: any } = matter(fileData)
+            if (!data.title || !data.date)
+                reject(Error("This file doesn't have proper headings."))
+            resolve({
+                slug: slug,
+                ...data
+            })
+        }).catch((err) => {
+            reject(err)
+        })
+    })
+}
+
+export async function getAllPosts() {
+    const entries = await readdir(config.posts_dir, { withFileTypes: true });
+    const fileContentsResult = await Promise.all(
+        entries.map((entry) => {
+            if (entry.isDirectory())
+                return readMarkdownFile(path.join(entry.path, entry.name, 'index.md'), entry.name).catch(e => e)
+            if (entry.name.endsWith('.md'))
+                return readMarkdownFile(path.join(entry.path, entry.name),
+                    entry.name.slice(0, -3)).catch(e => e)
+            return Error("Not a markdown Entry!")
+        })
+    )
+    return fileContentsResult.filter(result => !(result instanceof Error))
+}
+
+export async function getLatestPosts() {
+    const posts: PostData[] = await getAllPosts()
+    posts.sort((a: PostData, b: PostData) => {
+        if (a.date && b.date)
+            return Date.parse(a.date) < Date.parse(b.date) ? 1 : -1
+        else return 0
+    });
+    return posts
+}
+
+export async function getPost(slug: string): Promise<{ content: string, data: PostData }> {
+    const markdown_index_path = path.join(config.posts_dir, slug, 'index.md')
+    const markdown_file_path = path.join(config.posts_dir, slug + '.md')
+    const fileData = await readFile(markdown_index_path)
+        .catch(async () => await readFile(markdown_file_path))
+    const { content, data }: { content: string, data: any } = matter(fileData)
+    if (!data || !data.title)
+        throw Error("The file is empty or doesn't have proper headings")
+    return { content, data }
 }
--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -48,4 +48,54 @@ export async function generateStaticParams() {
     return posts.map((post) => ({
         slug: post.slug,
     }))
+}import { getPost } from "@/app/actions/postActions"
+import { notFound } from "next/navigation"
+import Markdown from "@/app/components/Markdown/Markdown"
+import { getAllPosts } from "@/app/actions/postActions";
+import { getMdAuthors, getMdDirection, getMdLanguage } from "@/app/actions/getMdProperties";
+import config from "@/config.blog";
+
+export async function generateMetadata({ params }: { params: { slug: string } }) {
+    const { data } = await getPost(params.slug).catch(() => notFound())
+
+    return {
+        title: data.title,
+        description: data.spoiler || `${config.blog_name}'s post`,
+        keywords: data.keywords || "",
+        authors: getMdAuthors(data),
+        openGraph: {
+            images: data.image || "",
+            publishedTime: data.date ? new Date(data.date).toISOString() : "",
+            type: "article",
+            authors: getMdAuthors(data)
+        }
+    }
+}
+
+export default async function Page({ params }: { params: { slug: string } }) {
+    const { data, content } = await getPost(params.slug).catch(() => notFound())
+
+    return <main className="min-h-screen py-4 px-4 md:px-2 pt-2 pb-16  max-w-2xl w-full" style={{ direction: getMdDirection(data) }}>
+        {data.image && <img src={data.image} alt={data.title} className="w-full mb-8 rounded-md aspect-[18/9.5]" />}
+        <h1 className="text-4xl mt-4 mb-2 block">{data.title}</h1>
+        <h1 className="text-sm sm:text-base">
+            {new Date(data.date || "").toLocaleDateString(getMdLanguage(data), {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+            })}
+        </h1>
+        <br />
+        <br />
+        <article className="markdown">
+            <Markdown source={content} />
+        </article>
+    </main>
+}
+
+export async function generateStaticParams() {
+    const posts = await getAllPosts()
+    return posts.map((post) => ({
+        slug: post.slug,
+    }))
 }
