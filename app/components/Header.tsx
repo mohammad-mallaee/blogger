07d64a491ae30117ba3db5ebe4be3d40e0addf62
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-08-28 20:48:56 +0330

    minor improvements

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,14 +9,14 @@ import { useTheme } from "next-themes";
 export default function Header() {
     const pathname = usePathname()
     const { theme, setTheme } = useTheme()
-    return <div className={`flex flex-col-reverse sm:flex-row w-full justify-between sm:items-center px-3 sm:px-0 pt-8 pb-6 max-w-[740px]`}
+    return <div className={`flex flex-wrap gap-4 sm:flex-row w-full justify-between sm:items-center px-4 md:px-0 pt-5 md:pt-8 pb-6 max-w-[740px]`}
         style={{ direction: config.direction }}>
-        <Link className={["text-3xl flex font-bold items-center gap-2", pathname === '/' ? "" : "text-on-background-variant"].join(" ")}
+        <Link className={["text-2xl sm:text-3xl flex font-bold items-center gap-2 md:gap-3", pathname === '/' ? "" : "text-on-background-variant"].join(" ")}
             href={'/'}>
-            {config.header.logo && <img className="size-11" src={config.logo} alt="logo" />}
+            {config.header.logo && <img className="size-8 sm:size-11" src={config.logo} alt="logo" />}
             {config.header.blog_name && config.blog_name}
         </Link>
-        <div className="flex gap-2 sm:gap-4 justify-end text-[24px] items-center">
+        <div className="flex gap-2 sm:gap-4 justify-end text-[24px] items-center grow">
             {config.header.nav_links && config.header.nav_links.length > 0 && config.header.nav_links.map((link: { href: string, name: string }) => {
                 const isHere = path.relative(pathname, link.href) === ''
                 return <Link key={link.href}
