99199314f2c65eee11754d263962246d78209fac
mohammad-mallaee <mohammad.mallaee@gmail.com>
2024-08-28 17:48:19 +0330

    fix layout shift caused by font loading

--- app/components/ThemeProvider.tsx
+++ app/components/ThemeProvider.tsx
@@ -2,6 +2,9 @@
 import { ReactNode, useState } from 'react'
 import config from '@/config.blog'
 import Header from './Header'
+import { Nunito } from "next/font/google"
+
+const nunito = Nunito({ subsets: ['latin'], weight: ['200', '300', '400', '500', '600', '700', '800', '900'] })
 
 const ThemeProvider = ({ children }: { children: ReactNode }) => {
 
@@ -27,7 +30,7 @@ const ThemeProvider = ({ children }: { children: ReactNode }) => {
 
   return (
     <body id="theme-provider"
-      className='flex flex-col items-center bg-[--background] text-[--on-background]'
+      className={'flex flex-col items-center bg-[--background] text-[--on-background] ' + nunito.className}
       data-theme={getTheme(theme)}>
       <Header theme={theme} setTheme={setTheme} />
       {children}
