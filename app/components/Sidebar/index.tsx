0cb2c72c39a8ffcd3238a2f84aa262dbe00f6db4
mohammad-mallaee <mohammad.mallaee@gmail.com>
2025-02-21 12:22:16 +0330

    add close button

--- app/components/Sidebar/index.tsx
+++ app/components/Sidebar/index.tsx
@@ -3,6 +3,7 @@ import { SidebarData } from "@/app/types";
 import Main from "./item";
 import { useContext } from "react";
 import SidebarContext from "../providers/sidebar";
+import { X } from "lucide-react";
 
 export default function Sidebar({ data }: { data: SidebarData }) {
     const context = useContext(SidebarContext)
@@ -13,7 +14,10 @@ export default function Sidebar({ data }: { data: SidebarData }) {
         <div className={`fixed top-0 lef-0 transition-opacity duration-300 ${context.isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
             <div className="fixed inset-0 bg-black/50 transition-opacity duration-300"
                 onClick={() => context.close()}></div>
-            <div className={`fixed top-0 left-0 z-10 h-screen max-h-screen overflow-scroll no-scrollbar w-full max-w-[320px] xl:hidden bg-background px-4 py-8 transform transition-transform duration-300 ${context.isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            <div className={`fixed top-0 left-0 z-10 h-screen max-h-screen overflow-scroll no-scrollbar w-full max-w-[320px] xl:hidden bg-background px-4 pb-8 pt-4 transform transition-transform duration-300 ${context.isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                <button className="block ml-auto mb-6 text-on-background-muted" onClick={() => context.close()}>
+                    <X />
+                </button>
                 <Main data={data} />
             </div>
         </div>
