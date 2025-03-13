60922cb6c126f13434fcbb19e436026ba5bb4fb4
Mohammad Mallaee <96518452+mohammad-mallaee@users.noreply.github.com>
2025-03-13 09:38:32 +0330

    improve sidebar discovery action and minor fixes (#17)

--- app/actions/sidebar.ts
+++ app/actions/sidebar.ts
@@ -1,5 +1,8 @@
-import { getAllPosts } from "./posts";
+import { getAllPosts, readPostData } from "./posts";
 import config from "@/config.blog";
+import { Dirent } from "fs";
+import fs from 'fs/promises'
+import { join, normalize, sep } from "path";
 
 export async function getPagesWithSidebar() {
     const entries = await getAllPosts({ recursive: true, path: "/", self: true })
@@ -12,24 +15,52 @@ export async function getPagesWithSidebar() {
     return result
 }
 
-export async function getSidebarData(slug = "/"): Promise<any> {
-    const entries = await getAllPosts({ recursive: false, path: slug, self: false })
-    if (entries.length === 0)
+function getSlug(entry: Dirent) {
+    let slug = entry.parentPath.replace(normalize(config.content_entry), "")
+    if (entry.name !== "index.md")
+        slug = join(sep, slug, entry.name.replace(".md", ""))
+    return slug
+}
+
+export async function getSidebarData(slug: string): Promise<any> {
+    const path = join(config.content_entry, slug)
+    const entries = await fs.readdir(path, { withFileTypes: true })
+    const files = entries.filter(entry => entry.isFile() && entry.name.endsWith(".md"))
+    const dirs = entries.filter(entry => entry.isDirectory())
+    const index = await readPostData(join(config.content_entry, slug, "index.md"), slug).catch(() => null)
+    if (files.length === 0 && dirs.length === 0 && !index)
         return null
     const result = []
-    for (const entry of entries) {
-        const data = await getSidebarData(entry.slug)
+    for (const file of files) {
+        if (file.name === "index.md")
+            continue
+        const s = getSlug(file)
+        const data = await readPostData(join(config.content_entry, slug, file.name), s)
         result.push({
-            url: entry.slug,
-            name: entry.title,
-            children: data,
-            order: entry.sidebar_order || Infinity
+            slug: s,
+            url: s,
+            order: data.sidebar_order || Infinity,
+            name: data.title || file.name.replace(".md", ""),
+            children: null,
         })
+    }
+    for (const dir of dirs) {
+        const children = await getSidebarData(join(slug, dir.name))
+        const data = await readPostData(join(config.content_entry, slug, dir.name, "index.md"), join(slug, dir.name))
+            .catch(() => null)
+        if (children && children.length !== 0 || data)
+            result.push({
+                slug: join(slug, dir.name),
+                url: data ? join(slug, dir.name) : null,
+                order: data ? data.sidebar_order || Infinity : Infinity,
+                name: data ? data.title || dir.name : dir.name,
+                children: children
+            })
     }
     result.sort((a, b) => {
         return a.order - b.order
     })
-    return result
+    return result.length === 0 ? null : result
 }
 
 export async function hasSidebar(slug: string) {
@@ -38,4 +69,3 @@ export async function hasSidebar(slug: string) {
     const pages: string[] = await getPagesWithSidebar()
     return pages.find(page => slug.startsWith(page))
 }
-
