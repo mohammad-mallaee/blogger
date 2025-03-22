cf11788f698e88881e71ef9fb2be3646ce2535e7
mohammad-mallaee <mohammad.mallaee@gmail.com>
2025-03-22 19:28:23 +0330

    add favicon to public

--- app/favicon.ico/route.ts
+++
@@ -1,6 +0,0 @@
-
-import config from '@/config'
-import { NextResponse } from 'next/server'
-export async function GET(request: Request) {
-  return NextResponse.redirect(new URL(config.logo, request.url))
-}
